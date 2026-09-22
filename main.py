af1157e8832ac7d9ccabd34b0e83f90f8fc68d0a
schmolioli <hkuikahi29@setoncatholic.org>
2026-09-22 20:16:36 +0000

    Guessing Game w AI

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import random
 
 game_number=random.randint(1,10)
+guess_count = 0
 
 while(True):
     guess=int(input("Guess a number between 1-10:"))
+    guess_count += 1
     if guess>game_number:
         print("too high")
     elif guess<game_number:
         print("too low")
     else:
-        print("correct!")
+        print(f"correct! It took you {guess_count} guesses.")
         break
